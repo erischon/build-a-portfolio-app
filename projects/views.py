3cd9eedcd0822326b7fd3d726f10853dbf7aee1b
Eri Schon <erischon@gmail.com>
2021-03-31 14:54:21 +0200

    Update views.py

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def project_list(request):
-    return HttpResponse("<h1>Hello world !</h1>")
+    return render(request, 'projects/index.html')
